669d157cae162257f35f433a14a95f03a764bcbf
GeniusXiao213 <100718931+GeniusXiao213@users.noreply.github.com>
2022-11-22 11:29:53 +0100

    part1

--- QuizManagementSystem/Form1.Designer.cs
+++ QuizManagementSystem/Form1.Designer.cs
@@ -28,22 +28,317 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSelectUser = new System.Windows.Forms.ComboBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.wrongLable = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnStudentRegister = new System.Windows.Forms.Button();
+            this.textEnrollementNo = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btnStudentLogin = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(266, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(758, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quiz/Exam Management System";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(506, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select User Type";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtSelectUser
+            // 
+            this.txtSelectUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSelectUser.ForeColor = System.Drawing.Color.Black;
+            this.txtSelectUser.FormattingEnabled = true;
+            this.txtSelectUser.Items.AddRange(new object[] {
+            "Student",
+            "Teacher"});
+            this.txtSelectUser.Location = new System.Drawing.Point(423, 155);
+            this.txtSelectUser.Name = "txtSelectUser";
+            this.txtSelectUser.Size = new System.Drawing.Size(391, 37);
+            this.txtSelectUser.TabIndex = 2;
+            this.txtSelectUser.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.Location = new System.Drawing.Point(1104, 48);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(63, 65);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-3, 255);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(758, 486);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(554, 318);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(8, 8);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.txtPassword);
+            this.panel1.Controls.Add(this.btnLogin);
+            this.panel1.Controls.Add(this.txtUserName);
+            this.panel1.Controls.Add(this.wrongLable);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(423, 231);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(393, 406);
+            this.panel1.TabIndex = 6;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(130, 240);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(155, 24);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Show Password";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.ForeColor = System.Drawing.Color.Black;
+            this.txtPassword.Location = new System.Drawing.Point(40, 202);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(331, 26);
+            this.txtPassword.TabIndex = 6;
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnLogin.FlatAppearance.BorderSize = 2;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(40, 274);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(333, 55);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.ForeColor = System.Drawing.Color.Black;
+            this.txtUserName.Location = new System.Drawing.Point(40, 102);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(331, 26);
+            this.txtUserName.TabIndex = 4;
+            this.txtUserName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // wrongLable
+            // 
+            this.wrongLable.AutoSize = true;
+            this.wrongLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wrongLable.ForeColor = System.Drawing.Color.Red;
+            this.wrongLable.Location = new System.Drawing.Point(82, 332);
+            this.wrongLable.Name = "wrongLable";
+            this.wrongLable.Size = new System.Drawing.Size(234, 20);
+            this.wrongLable.TabIndex = 3;
+            this.wrongLable.Text = "Wrong User Name Or Password";
+            this.wrongLable.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(158, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 22);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Password";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(158, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 22);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "User Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(126, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Teacher Login";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnStudentLogin);
+            this.panel2.Controls.Add(this.btnStudentRegister);
+            this.panel2.Controls.Add(this.textEnrollementNo);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(393, 406);
+            this.panel2.TabIndex = 8;
+            // 
+            // btnStudentRegister
+            // 
+            this.btnStudentRegister.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnStudentRegister.FlatAppearance.BorderSize = 2;
+            this.btnStudentRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStudentRegister.Location = new System.Drawing.Point(40, 274);
+            this.btnStudentRegister.Name = "btnStudentRegister";
+            this.btnStudentRegister.Size = new System.Drawing.Size(333, 55);
+            this.btnStudentRegister.TabIndex = 5;
+            this.btnStudentRegister.Text = "Register";
+            this.btnStudentRegister.UseVisualStyleBackColor = false;
+            // 
+            // textEnrollementNo
+            // 
+            this.textEnrollementNo.ForeColor = System.Drawing.Color.Black;
+            this.textEnrollementNo.Location = new System.Drawing.Point(39, 131);
+            this.textEnrollementNo.Name = "textEnrollementNo";
+            this.textEnrollementNo.Size = new System.Drawing.Size(331, 26);
+            this.textEnrollementNo.TabIndex = 4;
+            this.textEnrollementNo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(104, 102);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(200, 22);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Student Enrollement No";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(126, 25);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(146, 25);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Student Login";
+            // 
+            // btnStudentLogin
+            // 
+            this.btnStudentLogin.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnStudentLogin.FlatAppearance.BorderSize = 2;
+            this.btnStudentLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStudentLogin.Location = new System.Drawing.Point(39, 199);
+            this.btnStudentLogin.Name = "btnStudentLogin";
+            this.btnStudentLogin.Size = new System.Drawing.Size(333, 55);
+            this.btnStudentLogin.TabIndex = 6;
+            this.btnStudentLogin.Text = "Login";
+            this.btnStudentLogin.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1946, 1080);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1300, 740);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtSelectUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox txtSelectUser;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label wrongLable;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnStudentLogin;
+        private System.Windows.Forms.Button btnStudentRegister;
+        private System.Windows.Forms.TextBox textEnrollementNo;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
 }
 
--- QuizManagementSystem/Form1.cs
+++ QuizManagementSystem/Form1.cs
@@ -16,5 +16,81 @@ namespace QuizManagementSystem
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            panel2.Visible = false;
+            wrongLable.Visible = false;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(txtSelectUser.SelectedIndex==0) //choose student
+            {
+                panel1.Visible = false;
+                panel2.Visible = true;
+                
+            }else if(txtSelectUser.SelectedIndex==1)
+            {
+                panel2.Visible = false;
+                panel1.Visible = true;
+
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked==true) 
+            {
+                txtPassword.PasswordChar = '\0';
+                checkBox1.Text = "Hide Password";
+
+            }else if(checkBox1.Checked== false)
+            {
+                txtPassword.PasswordChar = '*';
+                checkBox1.Text = "Show Password";
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if(txtUserName.Text=="wan"&& txtPassword.Text == "xiaohan")
+            {
+                wrongLable.Visible = false;
+                Teacher te =new Teacher();
+                te.Show();
+                this.Hide();
+            }
+            else
+            {
+                wrongLable.Visible = true;
+            }
+        }
     }
 }
